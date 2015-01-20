39b40839db4394f7cf0f05316f7dca8d007fec83
Andrey <andrewart@yandex.ru>
2015-01-20 17:52:05 +0200

    5 pages

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(window).bind('scroll', function() {//Top header action
+	if ($(window).scrollTop() > 91) {
+		 $('.main_head').addClass('fixed');
+	}
+	else {
+		 $('.main_head').removeClass('fixed');
+	}
+});
+
+$(".username span").click(function () {// Show/hide user sub menu
+	$(this).parent().next(".profile_items").toggle();
+})
+
+$(".custom_select").dropkick({
+  mobile: true
+});
+
+$(".close").click(function () {
+	$(this).parent(".warning").slideUp();
+	$('.popup, .overlay').fadeOut(300);
+});
+
+/*-- Popup --*/
+$('#edit_geo').click(function () {
+	$('.overlay').fadeIn(300);
+	$('#pop2').fadeIn(300);
+});
+
+$('.overlay').click(function(){
+	$('.popup, .overlay').fadeOut(300);
+});
+/*-- End of Popup --*/
+
+$( document ).tooltip({ //jQueryUI tooltip
+  position: { my: "left bottom-12", at: "right-30 center" }
+});
+
+
+$('.geo_list label input[type="checkbox"]').bind('change', function () {// Show/hide nested uls on checking checkbox with geo location + toggle class for styling
+  if($(this).is(':checked')) {
+    $(this).parent("label").addClass('opened').parent("li").children("ul").show();
+  } else {
+    $(this).parent("label").removeClass('opened').parent("li").children("ul").hide();
+  }
+});
+
+
+$('.atable input[type="checkbox"]').bind('change', function () {// Hilight table row when checkbox is selected
+  if($(this).is(':checked')) {
+    $(this).parents("tr").addClass('selected_tr');
+  } else {
+    $(this).parents("tr").removeClass('selected_tr');
+  }
+});
